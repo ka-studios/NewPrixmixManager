5af0661db60f908b803f11b15bed1edb88c2d891
Karim Al-Ahmed <karimalahmed3@gmail.com>
2024-06-15 20:44:16 -0400

    comment stuff
    Signed-off-by: Karim Al-Ahmed <karimalahmed3@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
+# super lightweight alpine
 FROM alpine:latest
+# install stuff (do we really need ttf-dejavu?)
 RUN apk update && apk add --no-cache \
     dbus \
     ttf-dejavu \
@@ -15,6 +17,7 @@ RUN apk update && apk add --no-cache \
     py3-xdg \
     firefox
 RUN mkdir -p /var/log/supervisor
+# boilerplate
 RUN mkdir -p /home/prixmix/.vnc
 RUN adduser -D -g "" prixmix && \
     echo "prixmix:pxmxpwd" | chpasswd && \
@@ -22,12 +25,17 @@ RUN adduser -D -g "" prixmix && \
     echo pxmxpwd0 | vncpasswd -f > /home/prixmix/.vnc/passwd && \
     chown -R prixmix:prixmix /home/prixmix && \
     chmod 600 /home/prixmix/.vnc/passwd
+# shush its the vnc password
 RUN mkdir -p /home/prixmix/.config/openbox
+# boilerplate
 RUN echo "exec firefox" > /home/prixmix/.config/openbox/autostart
+# autostart firefox
 COPY supervisord.conf /etc/supervisor.conf
 EXPOSE 5904 6080
 COPY self.pem /home/prixmix/self.pem
+# ssl for novnc
 WORKDIR /home/prixmix
 USER root
+# copy policies file
 COPY policies.json /usr/lib/firefox/distribution/policies.json
 CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor.conf"]
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import threading
 client = docker.from_env()
 api = FastAPI()
 
+# stop containers after 2hr timeout (delay value added for future use with premium tier)
 def stopaftertimeout(container_id, delay):
     time.sleep(delay)
     container = client.containers.get(container_id)
@@ -24,10 +25,10 @@ async def create_container(request: Request, response: Response):
     novnc = random.randint(49153, 65560)
     vnc = random.randint(49153, 65550)
     while novnc == vnc:
-      vnc = random.randint(49153, 65550)
-    if data.get('auth') == "secretkey":
-        container = client.containers.run("newprixmix", ports={6080:novnc, 5904:vnc}, detach=True, mem_limit="512m")
-        stopthread = threading.Thread(target=stopaftertimeout, args=(container.id, 7200))
+      vnc = random.randint(49153, 65550) # make sure novnc port and vnc port do not conflict (yes its a 1/20000 chance but things happen sometimes)
+    if data.get('auth') == "secretkey": # change this pls
+        container = client.containers.run("newprixmix", ports={6080:novnc, 5904:vnc}, detach=True, mem_limit="512m") # mem limit 512 for now, will make it higher for premium
+        stopthread = threading.Thread(target=stopaftertimeout, args=(container.id, 7200)) # make it timeout after 2hr (need to modify this for premium)
         stopthread.start()
         return {
             "status":"success",
@@ -46,10 +47,10 @@ async def destroy_container(request: Request, response: Response):
     #    response.status_code = status.HTTP_401_UNAUTHORIZED
     data = await request.json()
     container_id = data.get('id')
-    if data.get('auth') == "secretkey":
+    if data.get('auth') == "secretkey": # check auth
         try:
             container = client.containers.get(container_id)
-            container.stop()
+            container.stop() # self explanatory
         except docker.errors.APIError:
             response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
             return {"status":"container not found"}
@@ -61,10 +62,10 @@ async def destroy_container(request: Request, response: Response):
 async def suspend_container(request: Request, response: Response):
     data = await request.json()
     container_id = data.get('id')
-    if data.get('auth') == "secretkey":
+    if data.get('auth') == "secretkey": # check auth
         try:
             container = client.containers.get(container_id)
-            container.pause()
+            container.pause() # self explanatory
         except docker.errors.APIError:
             response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
             return {"status":"container not found"}
@@ -75,10 +76,10 @@ async def suspend_container(request: Request, response: Response):
 async def resume_container(request: Request, response: Response):
     data = await request.json()
     container_id = data.get('id')
-    if data.get('auth') == "secretkey":
+    if data.get('auth') == "secretkey": # check auth
         try:
             container = client.containers.get(container_id)
-            container.unpause()
+            container.unpause() # self explanatory
         except docker.errors.APIError:
             response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
             return {"status":"container not found"}
@@ -88,15 +89,15 @@ async def resume_container(request: Request, response: Response):
 @api.post("/containers/killall")
 async def killall_containers(request: Request, response: Response):
     data = await request.json()
-    if data.get('auth') == "superdupersecret":
-        subprocess.call(["systemctl", "stop", "docker"])
-        subprocess.call(["systemctl", "stop", "containerd"])
+    if data.get('auth') == "superdupersecret": # Spooky!
+        subprocess.call(["systemctl", "stop", "docker"]) # not going to use the sdk for this one
+        subprocess.call(["systemctl", "stop", "containerd"]) # just in case
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"status":"not slick"}
+        return {"status":"not slick"} # ehehehe
 async def stream_reader(stream, url_pattern):
     while True:
-        line = await stream.readline()
+        line = await stream.readline() # stupid stream reader for zrok process
         if not line:
             break
         line = line.decode('utf-8').strip()
@@ -114,22 +115,22 @@ async def websockify_connect(request: Request, response: Response):
     try:
         data = await request.json()
     except:
-        return {"status":"missing auth token"}
+        return {"status":"missing auth token"} # check auth
     if not data.get('port'):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"status":"port missing from request"}
-    if data.get('auth') == "secretkey":
-        command = ["zrok", "share", "public", f"172.17.0.1:{data.get('port')}", "--headless"]
+    if data.get('auth') == "secretkey": # check auth
+        command = ["zrok", "share", "public", f"172.17.0.1:{data.get('port')}", "--headless"] # that ip BETTER not change randomly
         process = await asyncio.create_subprocess_exec(*command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         asyncio.create_task(terminate_process_after_delay(process, 7200))
-        url_pattern = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
+        url_pattern = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+') # very scuffed regex here
 
         async def read_output(stream):
             async for url in stream_reader(stream, url_pattern):
                 return url
 
         stdout_task = asyncio.create_task(read_output(process.stdout))
-        stderr_task = asyncio.create_task(read_output(process.stderr))
+        stderr_task = asyncio.create_task(read_output(process.stderr)) # use 2 threads becaus yes
 
 
         done, pending = await asyncio.wait([stdout_task, stderr_task], return_when=asyncio.FIRST_COMPLETED)
@@ -148,7 +149,7 @@ async def websockify_connect(request: Request, response: Response):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(api, host="127.0.0.1", port=5546)
+    uvicorn.run(api, host="127.0.0.1", port=5546) # uvicorn>>>>>>>>
 
 
 
