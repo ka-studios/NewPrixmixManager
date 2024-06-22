0c3c88ee63c87e9b3a1252292112c7966b8bc5c3
Karim Al-Ahmed <karimalahmed3@gmail.com>
2024-06-22 16:56:07 -0400

    ignore zeerok.py, was testing. also NO MORE ZROKKKK

--- main.py
+++ main.py
@@ -19,49 +19,12 @@ except docker.errors.DockerException:
 api = FastAPI()
 limiter = Limiter(key_func=get_remote_address, application_limits=["60/minute"])
 
-async def terminate_process_after_delay(process, delay):
-    await asyncio.sleep(delay)
-    process.terminate()
-
-async def stream_reader(stream, pattern):
-    while True:
-        line = await stream.readline()
-        if not line:
-            break
-        decoded_line = line.decode('utf-8')
-        match = pattern.search(decoded_line)
-        if match:
-            return match.group()
 
 # stop containers after 2hr timeout (delay value added for future use with premium tier)
 def stopaftertimeout(container_id, delay):
     time.sleep(delay)
     container = client.containers.get(container_id)
     container.stop()
-
-async def expose(port):
-        command = ["zrok", "share", "public", f"172.17.0.1:{port}", "--headless"] # that ip BETTER not change randomly
-        process = await asyncio.create_subprocess_exec(*command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        asyncio.create_task(terminate_process_after_delay(process, 7200))
-        url_pattern = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+') # very scuffed regex here
-
-        async def read_output(stream):
-            async for url in stream_reader(stream, url_pattern):
-                return url
-
-        stdout_task = asyncio.create_task(read_output(process.stdout))
-        stderr_task = asyncio.create_task(read_output(process.stderr)) # use 2 threads becaus yes
-
-
-        done, pending = await asyncio.wait([stdout_task, stderr_task], return_when=asyncio.FIRST_COMPLETED)
-
-
-        for task in pending:
-            task.cancel()
-
-
-        url = next(iter(done)).result()
-        return url
 
 @api.post("/containers/create")
 @limiter.limit("60/minute")
@@ -79,13 +42,10 @@ async def create_container(request: Request, response: Response):
         container = client.containers.run("newprixmix", ports={6080:novnc, 5904:vnc}, detach=True, mem_limit="512m", environment={"URL":recv_url})
         stopthread = threading.Thread(target=stopaftertimeout, args=(container.id, 7200)) # make it timeout after 2hr (need to modify this for premium)
         stopthread.start()
-        url = await expose(novnc)
         return {
             "status":"success",
             "container":container.id,
-            "novnc_port":novnc,
-            "vnc_port":vnc,
-            "zrok_url":url
+            "novnc_port":novnc
         }
 
     else:
@@ -185,7 +145,3 @@ async def authcheck(response: Response, request: Request):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(api, host="127.0.0.1", port=5546) # uvicorn>>>>>>>>
-
-
-
-
