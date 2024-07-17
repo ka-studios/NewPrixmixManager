e544d06406d53b2452b4b5e4d46437634e152e1e
Karim Al-Ahmed <karimalahmed3@gmail.com>
2024-07-17 19:41:37 -0400

    feat: switch to prisma, revamp entire manager

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM alpine:latest
 # install stuff (do we really need ttf-dejavu?)
 RUN apk update && apk add --no-cache \
     dbus \
+    apache2 \
     ttf-dejavu \
     bash \
     supervisor \
@@ -15,12 +16,14 @@ RUN apk update && apk add --no-cache \
     git \
     novnc \
     py3-xdg \
+    curl \
+    openssl \
     firefox
 RUN mkdir -p /var/log/supervisor
 # boilerplate
 RUN mkdir -p /home/prixmix/.vnc
 RUN adduser -D -g "" prixmix && \
-    echo "prixmix:pxmxpwd" | chpasswd && \
+    echo "prixmix:pxmxpwd0" | chpasswd && \
     chown -R prixmix:prixmix /home/prixmix && \
     echo pxmxpwd0 | vncpasswd -f > /home/prixmix/.vnc/passwd && \
     chown -R prixmix:prixmix /home/prixmix && \
@@ -39,4 +42,12 @@ WORKDIR /home/prixmix
 USER root
 # copy policies file
 COPY policies.json /usr/lib/firefox/distribution/policies.json
+# preload hosts file
+COPY hosts /etc/hosts
+#RUN rm -f /var/www/html/index.html
+#COPY blocked.html /var/www/html/index.html
+#COPY snakeoil.pem /etc/ssl/certs/ssl-cert-snakeoil.pem
+#COPY snakeoil.key /etc/ssl/private/ssl-cert-snakeoil.key
+#RUN a2enmod ssl
+#RUN cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/000-default.conf
 CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor.conf"]
--- main.py
+++ main.py
@@ -1,147 +1,181 @@
-from fastapi import FastAPI, Request, Response, WebSocket, WebSocketDisconnect, status
-from slowapi.errors import RateLimitExceeded
-from slowapi import Limiter, _rate_limit_exceeded_handler
+from fastapi import FastAPI, Request, Response, status
+from slowapi import Limiter
 from slowapi.util import get_remote_address
+from prisma import Prisma
 import docker
+import datetime
 import random
 import subprocess
 import asyncio
-import re
 import time
-import threading
+import json
+import logging
 import sys
+from pathlib import Path
+import uvicorn
+
+config = Path("config.json")
+if not config.is_file():
+    logging.error("Please create a config.json file with your secret key")
+    sys.exit(127)
+with open("config.json") as c:
+    data = c.read()
+    global auth
+    auth = json.loads(data).get('auth')
 
 try:
     client = docker.from_env()
 except docker.errors.DockerException:
     print("The Docker Daemon is not running.")
     sys.exit(1)
-api = FastAPI()
-limiter = Limiter(key_func=get_remote_address, application_limits=["60/minute"])
 
+api = FastAPI()
+limiter = Limiter(app=app, key_func=get_remote_address)
 
 # stop containers after 2hr timeout (delay value added for future use with premium tier)
 def stopaftertimeout(container_id, delay):
     time.sleep(delay)
-    container = client.containers.get(container_id)
-    container.stop()
+    container = client.containers.get(container_id)             
+    container.remove()
 
 @api.post("/containers/create")
 @limiter.limit("60/minute")
-async def create_container(request: Request, response: Response):
-
-    #if client !== "ip_here_later":
-    #    response.status_code = status.HTTP_401_UNAUTHORIZED
+async def create_container(request: Request, response: Response) -> None:
+    db = Prisma()
+    await db.connect()
     data = await request.json()
     novnc = random.randint(49153, 65560)
     vnc = random.randint(49153, 65550)
+    premium = data.get('premium', None)
     recv_url = data.get('url')
+    username = data.get('user')
+    if premium == 1:
+        ram = 1024
+    elif premium == 2:
+        ram = 2048
+    elif premium == 3:
+        ram = 4096
+    else:
+        pass
     while novnc == vnc:
-      vnc = random.randint(49153, 65550) # make sure novnc port and vnc port do not conflict (yes its a 1/20000 chance but things happen sometimes)
-    if data.get('auth') == "secretkey": # change this pls
-        container = client.containers.run("newprixmix", ports={6080:novnc, 5904:vnc}, detach=True, mem_limit="512m", environment={"URL":recv_url})
-        stopthread = threading.Thread(target=stopaftertimeout, args=(container.id, 7200)) # make it timeout after 2hr (need to modify this for premium)
-        stopthread.start()
-        return {
-            "status":"success",
-            "container":container.id,
-            "novnc_port":novnc
-        }
-
+        vnc = random.randint(49153, 65550)  # ensure novnc port and vnc port do not conflict
+    if data.get('auth') == auth:  # change this pls
+        container = client.containers.run("newprixmix", ports={6080:novnc, 5904:vnc}, detach=True, mem_limit=f"{ram}m", environment={"URL":recv_url})
+        await db.container.create(
+            {
+                'cid': container.id,
+                'expires': datetime.datetime.now() + datetime.timedelta(hours=premium+2),
+                'ram': ram,
+                'novnc': novnc,
+                'bearer': username,
+                'vnc': vnc,
+                'prem': False if not premium else True,
+            }
+        )
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"status":"auth token invalid"}
+        return {"status": "auth token invalid"}
 
 @api.post("/containers/destroy")
 @limiter.limit("60/minute")
 async def destroy_container(request: Request, response: Response):
-    #if client !== "ip_here_later":
-    #    response.status_code = status.HTTP_401_UNAUTHORIZED
     data = await request.json()
     container_id = data.get('id')
-    if data.get('auth') == "secretkey": # check auth
+    if data.get('auth') == auth:  # check auth
         try:
             container = client.containers.get(container_id)
-            container.stop() # self explanatory
+            container.stop()  # self explanatory
+            db = Prisma()
+            await db.connect()
+            await db.container.delete(where={'cid':container_id})
         except docker.errors.APIError:
             response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return {"status":"container not found"}
-
+            return {"status": "container not found"}
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"status":"auth token invalid"}
+        return {"status": "auth token invalid"}
+
 @api.post("/containers/suspend")
 @limiter.limit("60/minute")
 async def suspend_container(request: Request, response: Response):
     data = await request.json()
     container_id = data.get('id')
-    if data.get('auth') == "secretkey": # check auth
+    if data.get('auth') == auth:  # check auth
         try:
             container = client.containers.get(container_id)
-            container.pause() # self explanatory
+            container.pause()  # self explanatory
         except docker.errors.APIError:
             response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return {"status":"container not found"}
+            return {"status": "container not found"}
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"status":"auth token invalid"}
+        return {"status": "auth token invalid"}
+
 @api.post("/containers/resume")
 @limiter.limit("60/minute")
 async def resume_container(request: Request, response: Response):
     data = await request.json()
     container_id = data.get('id')
-    if data.get('auth') == "secretkey": # check auth
+    if data.get('auth') == auth:  # check auth
         try:
             container = client.containers.get(container_id)
-            container.unpause() # self explanatory
+            container.unpause()  # self explanatory
         except docker.errors.APIError:
             response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return {"status":"container not found"}
+            return {"status": "container not found"}
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"status":"auth token invalid"}
+        return {"status": "auth token invalid"}
+
 @api.post("/containers/killall")
 @limiter.limit("60/minute")
 async def killall_containers(request: Request, response: Response):
     data = await request.json()
-    if data.get('auth') == "superdupersecret": # Spooky!
-        subprocess.call(["systemctl", "stop", "docker"]) # not going to use the sdk for this one
-        subprocess.call(["systemctl", "stop", "containerd"]) # just in case
+    if data.get('auth') == "superdupersecret":  # Spooky!
+        db = Prisma()
+        await db.connect()
+        await db.container.delete_many(where={'prem':False})
+        await db.container.delete_many(where={'prem':True})
+        subprocess.call(["systemctl", "stop", "docker"])  # not going to use the sdk for this one
+        subprocess.call(["systemctl", "stop", "containerd"])  # just in case
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return {"status":"not slick"} # ehehehe
-async def stream_reader(stream, url_pattern):
-    while True:
-        line = await stream.readline() # stupid stream reader for zrok process
-        if not line:
-            break
-        line = line.decode('utf-8').strip()
-        urls = url_pattern.findall(line)
-        for url in urls:
-            yield url
-
-async def terminate_process_after_delay(process, delay):
-    await asyncio.sleep(delay)
-    process.terminate()
-    await process.wait()
+        return {"status": "not slick"}  # ehehehe
 
 @api.get("/_healthcheck")
 @limiter.limit("60/minute")
 async def healthcheck(response: Response, request: Request):
     response.status_code = status.HTTP_200_OK
     return Response("OK")
+
 @api.post("/_authcheck")
 @limiter.limit("60/minute")
 async def authcheck(response: Response, request: Request):
     data = await request.json()
-    if data.get('auth') == "secretkey":
+    if data.get('auth') == auth:
         response.status_code = status.HTTP_200_OK
         return Response("OK")
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return Response("Unauthorized")
+@api.post("/prune")
+@limiter.limit("3/minute")
+async def pruneContainers(request: Request, response: Response):
+    data = await request.json()
+    if data.get('auth') == auth:
+        db = Prisma()
+        await db.connect()
+        print("NP: Purging stale sessions")
+        found = await db.container.find_many(where={'expires': {'lt': datetime.datetime.now()}})
+        for container in found:
+            try:
+                c = client.containers.get(container.cid)
+                c.remove()
+                logging.info(f"Container {container.cid} removed successfully.")
+            except docker.errors.DockerException as e:
+                logging.error(f"Error removing container {container.cid}: {e}")
+        await db.container.delete_many(where={'expires': {'lt': datetime.datetime.now()}})
+        return Response("OK", status_code=200)
 
-        return url
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(api, host="127.0.0.1", port=5546) # uvicorn>>>>>>>>
+    uvicorn.run(api, host="127.0.0.1", port=5546)
